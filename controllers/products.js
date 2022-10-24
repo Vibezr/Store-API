56bc79a1803acd464a7cdea7cc80b7cf93f52744
Vibezr <vibezr35@gmail.com>
2022-10-24 18:11:45 +0100

    Second Commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,34 @@
-
+const Product = require('../models/product')
+const { search } = require('../routes/products')
 
 const getAllProductsStatic = async (req, res)=> {
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find({featured: true})
+    res.status(200).json({products, nbHits: products.length})
 }
 
+
 const getAllProducts = async (req, res)=> {
-    res.status(200).json({msg: 'products testing route'})
+    const {featured, company, name} = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = {$regex: name, $options: 'i'} 
+    }
+
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = { 
     getAllProducts,
-    getAllProductsStatic
+    getAllProductsStatic 
 }
